46711d720a3e3c8bf97cd44e2c88178205a4e357
Jonas Bernemann <jbernemann@gmx.de>
2020-01-18 01:51:11 +0100

    Add validation method for authorization

--- api/playlist.go
+++ api/playlist.go
@@ -2,14 +2,21 @@ package api
 
 import (
 	"fmt"
+	"github.com/cloudmusic-dev/backend/authorization"
 	"github.com/cloudmusic-dev/backend/database"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func handleListPlaylists(w http.ResponseWriter, r *http.Request) {
+	authorized, userId := authorization.ValidateRequest(r)
+	if !authorized {
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+
 	var playlists []database.Playlist
-	database.DB.Find(&playlists)
+	database.DB.Where("owner = ?", userId).Find(&playlists)
 
 	w.WriteHeader(http.StatusNotImplemented)
 	fmt.Fprintf(w, "%d", len(playlists))
--- authorization/main.go
+++ authorization/main.go
@@ -5,6 +5,10 @@ import (
 	"github.com/cloudmusic-dev/backend/configuration"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/google/uuid"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
 var Config configuration.Configuration
@@ -12,6 +16,8 @@ var Config configuration.Configuration
 func CreateToken(userId uuid.UUID) (string, error) {
 	claim := jwt.MapClaims{
 		"userId": userId.String(),
+		"nbf": time.Now().Unix(),
+		"exp": time.Now().AddDate(0, 0, 14).Unix(),
 	}
 
 	if Config.Signing.Method == "hmac" {
@@ -31,4 +37,45 @@ func CreateToken(userId uuid.UUID) (string, error) {
 	}
 
 	return "", fmt.Errorf("invalid signing method")
+}
+
+func ValidateRequest(r *http.Request) (bool, *uuid.UUID) {
+	authorization := r.Header.Get("Authorization")
+	if !strings.HasPrefix(authorization, "Bearer ") {
+		return false, nil
+	}
+
+	token := strings.TrimPrefix(authorization, "Bearer ")
+	return ValidateToken(token)
+}
+
+func ValidateToken(tokenString string) (bool, *uuid.UUID) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if Config.Signing.Method == "hmac" {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return []byte(Config.Signing.Key), nil
+		} else {
+			return nil, fmt.Errorf("invalid signing method")
+		}
+	})
+
+	if err != nil {
+		log.Printf("Failed to verify token: %v", err)
+		return false, nil
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		id, err := uuid.Parse(claims["userId"].(string))
+		if err != nil {
+			log.Printf("Failed to parse user id in jwt: %v", err)
+			return false, nil
+		}
+
+		return true, &id
+	} else {
+		return false, nil
+	}
 }
